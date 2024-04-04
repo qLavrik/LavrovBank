ef7bcb793cef0127c87d82c899f11fa7ada08560
qLAvrik <lyha.lavka03@gmail.com>
2024-04-04 17:35:43 +0300

    1212

--- src/main/java/bank/Card.java
+++ src/main/java/bank/Card.java
@@ -2,6 +2,7 @@ package bank;
 
 public class Card {
     private final long CARD_NUMBER;
+<<<<<<< HEAD
 
     private int pinCode;
 
@@ -23,13 +24,45 @@ public class Card {
 
     public void setPinCode(int pinCode) {
         this.pinCode = pinCode;
+=======
+    private String pinCode;
+
+    public Card(long CARD_NUMBER) {
+        this.CARD_NUMBER = CARD_NUMBER;
+        generatePinCode();
+    }
+
+    private void generatePinCode() {
+        StringBuilder generatingPin = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            generatingPin.append(rnd(0, 9));
+        }
+        pinCode = String.valueOf(Integer.parseInt(generatingPin.toString()));
+>>>>>>> 96a13fe (1212)
     }
 
     public long getCARD_NUMBER() {
         return CARD_NUMBER;
     }
+<<<<<<< HEAD
     public static int rnd(int min, int max){
         max -= min;
         return (int) (Math.random()* ++max) + min;
+=======
+
+    public String getPinCode() {
+        return pinCode;
+    }
+
+    public static void main(String[] args) {
+        Card firstCard = new Card(2208560858093670L);
+        System.out.println("Card Number: " + firstCard.getCARD_NUMBER());
+        System.out.println("PIN: " + firstCard.getPinCode());
+    }
+
+    public static int rnd(int min, int max) {
+        max -= min;
+        return (int) (Math.random() * ++max) + min;
+>>>>>>> 96a13fe (1212)
     }
 }
--- src/main/java/data/Client.java
+++ src/main/java/data/Client.java
@@ -1,4 +1,48 @@
 package data;
 
+<<<<<<< HEAD
 public class Client {
 }
+=======
+import bank.Card;
+
+public class Client {
+    private final String name;
+    private final String surname;
+    private Card card;
+    public Client(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+        generateCard();
+    }
+    private void generateCard() {
+        long cardNumber = generateCardNumber();
+        this.card = new Card(cardNumber);
+    }
+    private long generateCardNumber() {
+        StringBuilder cardNumberBuilder = new StringBuilder();
+        for (int i = 0; i < 16; i++) {
+            cardNumberBuilder.append(Card.rnd(0, 9));
+        }
+        return Long.parseLong(cardNumberBuilder.toString());
+    }
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public Card getCard() {
+        return card;
+    }
+    public static void main(String[] args) {
+        Client client = new Client("Alexey", "Lavrov");
+        System.out.println("Client Name: " + client.getName());
+        System.out.println("Client Surname: " + client.getSurname());
+        System.out.println("Card Number: " + client.getCard().getCARD_NUMBER());
+        System.out.println("PIN: " + client.getCard().getPinCode());
+    }
+}
+
+
+>>>>>>> 96a13fe (1212)
